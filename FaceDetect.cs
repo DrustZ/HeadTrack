5aaeccaeb3779f95bbcff3f189cddda14e020fbf
DrustZ <z1m6r3@gmail.com>
2017-03-17 12:03:02 +0800

    face detection add

---
+++ FaceDetect.cs
@@ -0,0 +1,51 @@
+﻿using Emgu.CV;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace HeadTrack
+{
+    class FaceDetect
+    {
+        private CascadeClassifier face = null;
+        Stopwatch watch;
+        Mat _grayFrame;
+
+        public FaceDetect()
+        {
+            _grayFrame = new Mat();
+            face = new CascadeClassifier("D:/Codes/Lab/HeadTracking/HeadTrack/haarcascade_frontalface_default.xml");
+        }
+
+        public void detectFace(Mat frame, bool debug = false)
+        {
+            if (debug)
+                watch = Stopwatch.StartNew();
+            CvInvoke.CvtColor(frame, _grayFrame, ColorConversion.Bgr2Gray);
+            CvInvoke.EqualizeHist(_grayFrame, _grayFrame);
+            Rectangle[] facesDetected = face.DetectMultiScale(
+                        _grayFrame,
+                        1.3,
+                        7,
+                        new System.Drawing.Size(70, 70));
+
+            foreach (Rectangle face in facesDetected)
+            {
+                CvInvoke.Rectangle(frame, face, new Bgr(System.Drawing.Color.Red).MCvScalar, 2);
+                
+            }
+            if (debug)
+            {
+                Console.WriteLine("find {0} faces!", facesDetected.Count());
+                watch.Stop();
+                Console.WriteLine("time elapse : {0}", watch.ElapsedMilliseconds);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using System.Diagnostics;
+
+namespace HeadTrack
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VideoCapture _capture = null;
+        private Mat _frame;
+        private bool _captureInProgress = false;
+        private FaceDetect detector = new FaceDetect();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new VideoCapture(1);
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            _frame = new Mat();
+            if (_capture != null) _capture.FlipHorizontal = !_capture.FlipHorizontal;
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+                detector.detectFace(_frame, true);
+                cambox.Image = _frame;
+
+            }
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_captureInProgress)
+            {
+                StartBtn.Content = "Start";
+                _capture.Pause();
+
+            } else
+            {
+                StartBtn.Content = "Stop";
+                _capture.Start();
+            }
+            _captureInProgress = !_captureInProgress;
+        }
+
+        
+    }
+}
